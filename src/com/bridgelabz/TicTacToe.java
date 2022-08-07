dc9707ab17b57a06906c0d2beafc72f21dc7314c
Sam5196 <sammedpachore1008@gmail.com>
2022-08-07 10:44:35 +0530

    Added:Choose user input program

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,9 +1,13 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class TicTacToe {
+    static char x='X';
+    static char o='O';
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         boardCreation();
+        chooseUserInput();
     }
 
     public static void boardCreation() {
@@ -12,5 +16,21 @@ public class TicTacToe {
             element[i] = ' ';
         }
         System.out.println("Tic Tac Toe board is ready");
+    }
+
+    public static void chooseUserInput() {
+
+        System.out.println("Input between X or O");
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        sc.close();
+
+        if (ch == 'x' || ch == 'o')
+            System.out.println(" you have entered: " + x);
+        else if (ch == 'X' || ch == 'O')
+            System.out.println("you have entered: " + o);
+        else
+            System.out.println("invalid input ");
+
     }
 }
